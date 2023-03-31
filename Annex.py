e3360ab7eefe95a09f8aeaba78da65528d3201d1
Ankita <ankita0512ghosh@gmail.com>
2023-03-31 17:33:16 +0530

    Added Tkinter frontend

---
+++ Annex.py
@@ -0,0 +1,137 @@
+import datetime,subprocess,os,pyautogui,string,random,time
+import pyttsx3
+import speech_recognition as sr
+import sounddevice,pywhatkit
+from scipy.io.wavfile import write
+from tkinter import filedialog
+import tkinter as tk
+from tkinter import ttk
+import pyperclip,cv2,playsound,requests,json 
+from ttkthemes import themed_tk as tkth
+import tkinter.scrolledtext as scrolledtext
+from functools import partial
+import tkinter.messagebox as tmsg,sqlite3
+
+class SpeakRecog:
+
+    def __init__(self,scrollable_text):
+        self.scrollable_text=scrollable_text
+    #database connection
+    conn = sqlite3.connect('Avsar.db')
+    mycursor=conn.cursor()
+
+    engine=pyttsx3.init('sapi5')
+    voices=engine.getProperty('voices')
+    engine.setProperty('voice',voices[1].id)
+    # print(voices[].id)
+    # print(voices)
+
+    """ VOICE RATE"""
+    rate = engine.getProperty('rate')               # getting details of current speaking rate
+    # print(rate)
+    engine.setProperty('rate',mycursor.execute('select rate from speech_rate').fetchone()[0])                 # setting up new voice rate in words per minute
+
+    """VOLUME"""   
+    volume = engine.getProperty('volume')           #getting to know current volume level (min=0 and max=1)
+    # print(volume)                                 #printing current volume level
+    engine.setProperty('volume',(mycursor.execute('select vol from volume').fetchone()[0])/10)               # setting up volume level  between 0 and 1
+    conn.commit()
+    conn.close()
+    scrollable_text=None
+    def STS(self,scrollable_text):
+        '''This is scrollable text setter '''
+        self.scrollable_text=scrollable_text
+    def updating_ST(self,data):
+        self.scrollable_text.configure(state='normal')
+        self.scrollable_text.insert('end',data+'\n')
+        self.scrollable_text.configure(state='disabled')
+        self.scrollable_text.see('end')
+        self.scrollable_text.update()
+    def updating_ST_No_newline(self,data):
+        self.scrollable_text.configure(state='normal')
+        self.scrollable_text.insert('end',data)
+        self.scrollable_text.configure(state='disabled')
+        self.scrollable_text.see('end')
+        self.scrollable_text.update()
+    def scrollable_text_clearing(self):
+        self.scrollable_text.configure(state='normal')
+        self.scrollable_text.delete(1.0,'end')
+        self.scrollable_text.configure(state='disabled')
+        self.scrollable_text.update()
+    def speak(self,audio):
+        """It speaks the audio"""
+        self.updating_ST(audio)
+        self.engine.say(audio)
+        # engine.save_to_file('Hello World', 'test.mp3')
+        self.engine.runAndWait()
+        # engine.stop()
+
+    def nonPrintSpeak(self,audio):
+        self.engine.say(audio)
+        self.engine.runAndWait()
+
+    def takeCommand(self):
+        """It take microphone input from the user and return string"""
+        recog=sr.Recognizer()
+        # mic=Microphone()
+        with sr.Microphone() as source:
+            #r.adjust_for_ambient_noise(source)
+            self.updating_ST("\nListening...")
+            recog.pause_threshold = 1
+            # r.energy_threshold = 45.131829621150224
+            # print(sr.Microphone.list_microphone_names())
+            #print(r.energy_threshold)
+            audio=recog.listen(source)
+        try:
+            self.updating_ST("Recognizing...")
+            query= recog.recognize_google(audio)
+            self.updating_ST(f"You: {query}\n")
+        except Exception as e:
+            # print(e)
+            self.updating_ST("Say that again please...")
+            return 'None'
+        return query
+
+class SettingWindow:
+    def Apply(self):
+        #Database connection
+        conn = sqlite3.connect('Avsar.db')
+        mycursor=conn.cursor()
+        Speech_Rate=self.speech_rate_text_box.get()
+        if not (Speech_Rate.isdigit()):
+            tmsg.showinfo("Error.",f"Please enter integers.")
+            self.setting.focus_force()
+        else:
+            mycursor.execute('update speech_rate set rate=?',(int(Speech_Rate),))
+            value=int((self.volume_slider.get()))
+            mycursor.execute('update volume set vol=?',(value,))
+            conn.commit()
+            conn.close()
+            # print(f"{value} type is {type(value)}")
+            tmsg.showinfo("Point to be noted.",f"Setting will be applied after reboot of this program.")
+            self.setting.destroy()
+    def settingWindow(self,root):
+        #database connection
+        conn = sqlite3.connect('Avsar.db')
+        mycursor=conn.cursor()
+        self.setting=tk.Toplevel(root)
+        canvas=tk.Canvas(self.setting)
+        canvas.create_line(0,135,285,135)
+        canvas.create_line(0,138,285,138)
+        canvas.pack()
+        self.setting.title("Settings")
+        self.setting.iconbitmap('setting.ico')
+        self.setting.geometry("285x180+500+200")
+        self.setting.resizable(0,0)
+        self.volume=ttk.Label(self.setting,text="Dork's Volume: ",borderwidth=0,font=('"Times New Roman"')).place(x=3,y=17)
+        self.volume=ttk.Label(self.setting,text='Speech Rate[WPM]:',borderwidth=0,font=('"Times New Roman"')).place(x=3,y=77)
+        self.volume_slider=tk.Scale(self.setting,from_=0,to=10,orient=tk.HORIZONTAL)
+        Integer_class=tk.IntVar(self.setting,value=mycursor.execute('select rate from speech_rate').fetchone()[0])
+        self.speech_rate_text_box=ttk.Entry(self.setting,textvariable=Integer_class)
+        self.volume_slider.place(x=137,y=0)
+        self.speech_rate_text_box.place(x=140,y=77)
+        self.volume_slider.set((mycursor.execute('select vol from volume').fetchone()[0]))
+        conn.commit()
+        conn.close()
+        self.Apply_Button=ttk.Button(self.setting,text="Apply",command=self.Apply).place(x=200,y=150)
+        self.setting.mainloop()
--- keywordtoanswer.py
+++ keywordtoanswer.py
@@ -1,4 +1,9 @@
 import csv
+from ttkthemes import themed_tk
+from tkinter import ttk
+import tkinter as tk
+from tkinter import scrolledtext
+from PIL import ImageTk,Image
 from rake_nltk import Rake
 from random import randint
 import os
@@ -7,88 +12,147 @@ import playsound
 import sys
 import pyttsx3
 from flask import Flask,render_template
-
-
-engine=pyttsx3.init()
-engine.say("Good Afternoon")
-engine.say("What is your name")
-engine.runAndWait()
-def talk(text):
-    engine.say(text)
-    engine.runAndWait()
+import Annex
+from functools import partial
+import urllib.request,bs4 as bs,sys,threading
 
 import speech_recognition as sr
 from gtts import gTTS
 
-sys.getrecursionlimit() # Prints 1000
-print("speak now")
-def speak(text):
-    tts=gTTS(text=all, lang="en",tld="com")
-    filename="onlymp3.to - One of the Greatest Speeches Ever  Steve Jobs-Tuw8hxrFBH8-192k-1659983637536"
-    tts.save(filename)
-    playsound.playsound(filename)
+def there_exists(terms,query):
+    for term in terms:
+        if term in query:
+            return True
 
-def get_audio():
-    r=sr.Recognizer()
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-        said = ""
-        try:
-            said = r.recognize_google(audio)
-            print(said)
-        except Exception as e:
-            print("Exception: " + str(e))
-    return said
+def mainframe():
+    engine=pyttsx3.init()
+    #engine.say("Good Afternoon")
+    SR.speak("Good Afternoon")
+    SR.speak("What is your name")
+    #SR.runAndWait()
+    def talk(text):
+        SR.speak(text)
+        #SR.runAndWait()
+    sys.getrecursionlimit() # Prints 1000
+    SR.takeCommand()
+    def speak(text):
+        tts=gTTS(text=all, lang="en",tld="com")
+        filename="onlymp3.to - One of the Greatest Speeches Ever  Steve Jobs-Tuw8hxrFBH8-192k-1659983637536"
+        tts.save(filename)
+        playsound.playsound(filename)
 
-text = get_audio()
+    def get_audio():
+        r=sr.Recognizer()
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+            said = ""
+            try:
+                said = r.recognize_google(audio)
+                SR.takeCommand()
+            except Exception as e:
+                print("Exception: " + str(e))
+        return said
+
+    text = get_audio()
 #if "hello" in text:
  #   speak("hello how are you")
 
-engine.say("Tell me something about yourself")
-engine.runAndWait()
+    SR.speak("Tell me something about yourself")
+    #SR.runAndWait()
 
 
-print("listening.........")
-text=get_audio()
+    SR.takeCommand()
+    text=get_audio()
 
-r = Rake()
-file = open('C:/Users/Dell/Dropbox/PC/Desktop/COLLEGE/chatbot/venv/dataset.csv')
-csvreader = csv.reader(file)
-header = []
-header = next(csvreader)
-print(header)
-rows = []
-for row in csvreader:
-     rows.append(row)
+    r = Rake()
+    file = open('dataset.csv')
+    csvreader = csv.reader(file)
+    header = []
+    header = next(csvreader)
+    print(header)
+    rows = []
+    for row in csvreader:
+         rows.append(row)
 
-print(rows)
-
-
-print("listening.........")
-text=get_audio()
-
-for qno in range(0,3):
-     index = randint(0,29)
-     question =rows[index][0]
-     engine.say(question)
-     engine.runAndWait()
-     answer = rows[index][1]
-     print("Give answer")
-     text = get_audio()
-     r=Rake()
-     r.extract_keywords_from_text(answer)
-
-     for rating,keyword in r.get_ranked_phrases_with_scores():
-         ans = keyword.split()
-         for keyword1 in ans:
-             if keyword1 not in text:
-                 print("please speak this keyword too",keyword1)
-         if rating > 5:
-            print(rating, ans)
+    print(rows)
 
 
-engine.say("nice to meet you")
-print("nice to meet you")
+    SR.takeCommand()
+    text=get_audio()
 
+    for qno in range(0,3):
+        index = randint(0,29)
+        question =rows[index][0]
+        SR.speak(question)
+        #SR.runAndWait()
+        answer = rows[index][1]
+        SR.speak("Give answer")
+        text = get_audio()
+        r=Rake()
+        r.extract_keywords_from_text(answer)
+
+        for rating,keyword in r.get_ranked_phrases_with_scores():
+            ans = keyword.split()
+            for keyword1 in ans:
+                if keyword1 not in text:
+                    SR.speak("please speak this keyword too",keyword1)
+            if rating > 5:
+                SR.speak(rating, ans)
+
+
+    SR.speak("nice to meet you")
+    SR.speak("nice to meet you")
+
+
+def clearScreen():
+    ''' clear the scrollable text box'''
+    SR.scrollable_text_clearing()
+
+def gen(n):
+    for i in range(n):
+        yield i
+
+class MainframeThread(threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        mainframe()
+
+def Launching_thread():
+    Thread_ID=gen(1000)
+    global MainframeThread_object
+    MainframeThread_object=MainframeThread(Thread_ID.__next__(),"Mainframe")
+    MainframeThread_object.start()
+
+if __name__=="__main__":
+        #tkinter code
+        root=themed_tk.ThemedTk()
+        root.set_theme("winnative")
+        root.geometry("{}x{}+{}+{}".format(920,360,int(root.winfo_screenwidth()/2 - 920/2),int(root.winfo_screenheight()/2 - 360/2)))
+        root.resizable(0,0)
+        root.title("Avsar")
+        #root.iconbitmap('Dork.ico')
+        root.configure(bg='#2c4557')
+        scrollable_text=scrolledtext.ScrolledText(root,state='disabled',height=15,width=87,relief='sunken',bd=5,wrap=tk.WORD,bg='#add8e6',fg='#800000')
+        scrollable_text.place(x=10,y=10)
+        mic_img=Image.open("Mic.png")
+        mic_img=mic_img.resize((55,55),Image.ANTIALIAS)
+        mic_img=ImageTk.PhotoImage(mic_img)
+        Speak_label=tk.Label(root,text="SPEAK:",fg="#FFD700",font='"Times New Roman" 12 ',borderwidth=0,bg='#2c4557')
+        Speak_label.place(x=250,y=300)
+        """Setting up objects"""
+        SR=Annex.SpeakRecog(scrollable_text)    #Speak and Recognition class instance
+        Listen_Button=tk.Button(root,image=mic_img,borderwidth=0,activebackground='#2c4557',bg='#2c4557',command=Launching_thread)
+        Listen_Button.place(x=330,y=280)
+        myMenu=tk.Menu(root)
+        m1=tk.Menu(myMenu,tearoff=0) #tearoff=0 means the submenu can't be teared of from the window
+        myMenu.add_cascade(label="Help",menu=m1)
+        stng_win=Annex.SettingWindow()
+        myMenu.add_cascade(label="Settings",command=partial(stng_win.settingWindow,root))
+        myMenu.add_cascade(label="Clear Screen",command=clearScreen)
+        root.config(menu=myMenu)
+        root.mainloop()
 
 
